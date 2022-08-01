6f657d2107a2376317f8a10d779c4851494eebc3
Alexis Mainwaring <leximainw@gmail.com>
2022-08-01 12:58:43 -0400

    add customer names

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+const displayTitles = false
+
+function capitalize(text)
+{
+    // vvv NO! (strings are indexed as UTF-16, so this can split Unicode codepoints)
+    // return text[0].toUpperCase() + text.substring(1)
+
+    // instead, destructuring a string gives codepoints
+    if (!text.length)
+        return ''
+    const chars = [...text]
+    return chars[0].toUpperCase() + chars.slice(1).join('')
+}
+
+function formatName(name)
+{
+    let text = `${capitalize(name.first)} ${capitalize(name.last)}`
+    if (displayTitles && name.title)
+        text = `${capitalize(name.title)}. ${text}`
+    return text
+}
+
 function pushCustomer(customer)
 {
     const elem = document.createElement('div')
@@ -6,6 +28,10 @@ function pushCustomer(customer)
     const picture = customer.picture
     img.src = picture.large || picture.medium || picture.thumbnail;   // prefer largest image
     elem.appendChild(img)
+    const name = document.createElement('div')
+    name.classList.add('name')
+    name.innerText = formatName(customer.name)
+    elem.appendChild(name)
     document.querySelector('#customers').appendChild(elem)
 }
 
