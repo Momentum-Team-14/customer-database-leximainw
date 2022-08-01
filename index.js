6737598440f933685a16c23d76d365a29a462cda
Alexis Mainwaring <leximainw@gmail.com>
2022-08-01 13:22:49 -0400

    add location and dates

--- index.js
+++ index.js
@@ -1,18 +1,42 @@
 const displayTitles = false
 
+const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+]
+
 function capitalize(text)
 {
     if (!text.length)
     {
         return ''
     }
-    
+
     // vvv NO! (strings are indexed as UTF-16, so this can split Unicode codepoints)
     // return text[0].toUpperCase() + text.substring(1)
 
     // instead, destructuring a string gives codepoints
     const chars = [...text]
     return chars[0].toUpperCase() + chars.slice(1).join('')
+}
+
+function formatDate(date)
+{
+    if (typeof(date) === 'string')
+    {
+        date = new Date(date)
+    }
+    return `${months[date.getMonth()].substring(0, 3)} ${date.getDate()} ${date.getFullYear()}`
 }
 
 function formatName(name)
@@ -27,14 +51,50 @@ function pushCustomer(customer)
 {
     const elem = document.createElement('div')
     elem.classList.add('customer')
-    const img = document.createElement('img')
     const picture = customer.picture
-    img.src = picture.large || picture.medium || picture.thumbnail;   // prefer largest image
-    elem.appendChild(img)
-    const name = document.createElement('div')
-    name.classList.add('name')
-    name.innerText = formatName(customer.name)
-    elem.appendChild(name)
+    let child = document.createElement('img')
+    child.src = picture.large || picture.medium || picture.thumbnail;   // prefer largest image
+    elem.appendChild(child)
+    child = document.createElement('div')
+    child.classList.add('name')
+    child.innerText = formatName(customer.name)
+    elem.appendChild(child)
+    if (customer.email)
+    {
+        child = document.createElement('div')
+        child.classList.add('contact-info')
+        child.innerText = customer.email
+        elem.appendChild(child)
+    }
+    if (customer.location)
+    {
+        const location = customer.location
+        if (location.street)
+        {
+            const street = location.street
+            child = document.createElement('div')
+            child.innerText = `${street.number} ${street.name}`
+            elem.appendChild(child)
+        }
+        child = document.createElement('div')
+        const stateCode = (usStates.find(x => x.name === location.state)
+            || {abbreviation: location.state})
+            .abbreviation
+        child.innerText = `${location.city} ${stateCode} ${location.postcode}`
+        elem.appendChild(child)
+    }
+    if (customer.dob)
+    {
+        child = document.createElement('div')
+        child.innerText = `DOB: ${formatDate(customer.dob.date)}`
+        elem.appendChild(child)
+    }
+    if (customer.registered)
+    {
+        child = document.createElement('div')
+        child.innerText = `Customer since: ${formatDate(customer.registered.date)}`
+        elem.appendChild(child)
+    }
     document.querySelector('#customers').appendChild(elem)
 }
 
